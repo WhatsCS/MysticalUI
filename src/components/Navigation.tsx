b3effc31ef6e4c925e7294cc8b96ad44a2cd566b
WhatsCS <whatscs@jointheb.org>
2019-01-01 13:39:08 -0800

    adjusting for material and code layout

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+export default function NavMenu() {
+  return (
+    <div className="NavigationMenu">
+      <AppBar>
+        <Toolbar />
+      </AppBar>
+    </div>
+  );
+}
