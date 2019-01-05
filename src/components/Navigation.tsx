531c9ce3bdf691b0448f39ce3e7522e20e448fa2
WhatsCS <whatscs@jointheb.org>
2019-01-05 12:17:49 -0800

    state prep and stuff

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,31 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, IconButton, Icon, Button } from "./Imports";
+import React, { Component } from "react";
+import { Container, Menu, Grid, Dropdown } from "semantic-ui-react";
 
-export default function NavMenu() {
-  return (
-    <div className="NavigationMenu">
-      <AppBar>
-        <Toolbar>
-          <IconButton color="secondary">
-            <Icon>home</Icon>
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            MysticalUI
-          </Typography>
-          <Button>Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+export default class NavBar extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Menu borderless fixed="top" fluid>
+        <Menu.Item header>MysticalUI</Menu.Item>
+        <Menu.Menu borderless position="right">
+          <Menu.Item header>
+            <a href="#">Login</a>
+          </Menu.Item>
+          <Menu.Item>
+            <Dropdown icon="user" text="<user.name>" labeled>
+              <Dropdown.Menu>
+                <Dropdown.Item text="Profile" />
+                <Dropdown.Item text="Uploads" />
+                <Dropdown.Divider />
+                <Dropdown.Item text="Logout" />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
 }
