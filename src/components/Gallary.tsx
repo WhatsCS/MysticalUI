85416e1db2329d8b9ce0a35659b04c5cd8b4d39b
WhatsCS <whatscs@jointheb.org>
2019-01-09 12:18:24 -0800

    placeholder logic for the segment

--- src/components/Gallary.tsx
+++ src/components/Gallary.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Container, Segment } from "semantic-ui-react";
+import { Card, Container, Segment, Header } from "semantic-ui-react";
 import UploadCard from "./Card";
 import "./Gallary.css";
 
@@ -61,18 +61,29 @@ export default class Gallary extends Component {
     },
   ];
 
-  listCards = this.prop.map(prop => <UploadCard upload={prop} />);
-
   render() {
+    let listCards = this.prop.map(prop => <UploadCard upload={prop} />);
+    let cards = (
+      <Card.Group doubling itemsPerRow="5">
+        {listCards}
+      </Card.Group>
+    );
+
+    let segment;
+    if (this.prop.length === 0) {
+      segment = (
+        <Segment placeholder>
+          <Header textAlign="center">No uploads listed, please upload something...</Header>
+        </Segment>
+      );
+    } else {
+      segment = <Segment>{cards}</Segment>;
+    }
+
     return (
       <Container className="main">
         {/* have a place holder for when nothing has been uploaded */}
-        <Segment>
-          <Card.Group doubling itemsPerRow="5">
-            {/* pass through the first page of uploads */}
-            {this.listCards}
-          </Card.Group>
-        </Segment>
+        {segment}
       </Container>
     );
   }
