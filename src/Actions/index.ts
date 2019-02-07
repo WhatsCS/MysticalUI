d9a380dbfcb96862543733f2b3791885e5ebd1f8
WhatsCS <whatscs@jointheb.org>
2019-02-06 17:22:29 -0800

    begin redux devel

---
+++ src/Actions/index.ts
@@ -0,0 +1,35 @@
+// Actions file, all action signals will be defined and documented here
+
+/* 
+    RequestPage is used to simply tell react that you are requesting a new page.
+    Usage: Pagination
+ */
+export const requestPage = (page: number) => ({
+  type: "REQUEST_PAGE",
+  payload: {
+    page
+  }
+});
+
+/* 
+    RecievePage is uesd to return the page number for the pagination component,
+    and the array of uploads for that next page.
+    Usage: Pagination
+*/
+export const recievePage = (page: number, uploads: Array<string>) => ({
+  type: "RECIEVE_PAGE",
+  payload: {
+    page,
+    uploads
+  }
+});
+
+/*
+    Authenticate 
+*/
+export const signOut = (authed: boolean) => ({
+  type: "SIGN_OUT",
+  payload: {
+    authed
+  }
+});
